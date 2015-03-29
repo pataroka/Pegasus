a50c81b80612ce3283410573bdf794f1562ff356
Velizar <zytobg@gmail.com>
2015-03-29 18:26:58 +0300

    fixed clickable start

--- js/game.js
+++ js/game.js
@@ -5,7 +5,7 @@
 var startButton = new Image();
 startButton.src = 'assets/images/Start_Game_Sprite.png';
 ctx.drawImage(startButton, -150, 50, 800, 600);
-document.getElementById('start-button').onclick = function () {
+document.getElementById('start-button-clicked').onclick = function () {
     startGame();
 };
 
